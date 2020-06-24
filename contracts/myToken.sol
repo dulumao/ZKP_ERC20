aafd20365e6286ceded65d28029936b3b3643624
AntoineMkr <ant.mecker@hotmail.fr>
2020-06-24 15:35:35 +0200

    :construction: working on contracts

--- contracts/myToken.sol
+++ contracts/myToken.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.6.0;
 
 
 import "node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "./verifier.sol";
 
 contract myToken is ERC20 {
 
@@ -19,23 +20,43 @@ contract myToken is ERC20 {
 
 
     // IL FAUT APPELER LES FONCTIONS DU CONTRAT VERIFIER.SOL
-    
+    address addressVerifier;
+    function setAddressVerif(address _addressVerif) external {
+        addressVerifier = _addressVerif;
+    }
+
+    function callVerifier(uint[2] memory a,uint[2][2] memory b,uint[2] memory c,uint[1] memory input, uint personne) private view returns(bool) {
+        InterfaceVerifier v = InterfaceVerifier(addressVerifier);
+        if(personne == 1){
+            return v.verifyProofSender(a,b,c,input);
+        }
+        if(personne == 2){
+            return v.verifyProofReceiver(a,b,c,input);
+        }
+    }
+
+
     // function transfer(address _to, bytes32 hashValue, bytes32 hashSenderBalanceAfter, bytes32 hashReceiverBalanceAfter,
     //     bytes zkProofSender, bytes zkProofReceiver) public
-    // {
-    //     bytes32 hashSenderBalanceBefore = balanceHashes[msg.sender];
-    //     bytes32 hashReceiverBalanceBefore = balanceHashes[_to];
 
-    //     bool senderProofIsCorrect = zksnarkverify(confTxSenderVk, [hashSenderBalanceBefore, hashSenderBalanceAfter, hashValue], zkProofSender);
+    function transfer(uint[2] memory a,uint[2][2] memory b,uint[2] memory c,uint[1] memory input) public view returns(bool r)
+    {
+        // bytes32 hashSenderBalanceBefore = balanceHashes[msg.sender];
+        // bytes32 hashReceiverBalanceBefore = balanceHashes[_to];
 
-    //     bool receiverProofIsCorrect = zksnarkverify(confTxReceiverVk,
-    //         [hashReceiverBalanceBefore, hashReceiverBalanceAfter, hashValue], zkProofReceiver);
+        bool senderProofIsCorrect = callVerifier(a,b,c,input,1);
+        bool receiverProofIsCorrect = callVerifier(a,b,c,input,2);
 
-    //     if(senderProofIsCorrect && receiverProofIsCorrect) {
-    //         balanceHashes[msg.sender] = hashSenderBalanceAfter;
-    //         balanceHashes[_to] = hashReceiverBalanceAfter;
-    //     }
-    // }
+        // if(senderProofIsCorrect) {
+            // balanceHashes[msg.sender] = hashSenderBalanceAfter;
+            // balanceHashes[_to] = hashReceiverBalanceAfter;
+        // }
+        r = false;
+        if(senderProofIsCorrect && receiverProofIsCorrect){
+            r = true;
+        }
+        return r;
+    }
 
 }
 
--- contracts/verifier.sol
+++ contracts/verifier.sol
@@ -52,7 +52,7 @@ library Pairing {
             return G1Point(0, 0);
         return G1Point(p.X, q - (p.Y % q));
     }
-    /// @return the sum of two points of G1
+    ///  the sum of two points of G1
     function addition(G1Point memory p1, G1Point memory p2) internal view returns (G1Point memory r) {
         uint[4] memory input;
         input[0] = p1.X;
@@ -62,13 +62,13 @@ library Pairing {
         bool success;
         // solium-disable-next-line security/no-inline-assembly
         assembly {
-            success := staticcall(sub(gas, 2000), 6, input, 0x80, r, 0x60)
+            success := staticcall(sub(gas(), 2000), 6, input, 0xc0, r, 0x60)
             // Use "invalid" to make gas estimation work
             switch success case 0 { invalid() }
         }
         require(success,"pairing-add-failed");
     }
-    /// @return the product of a point on G1 and a scalar, i.e.
+    /// the product of a point on G1 and a scalar, i.e.
     /// p == p.scalar_mul(1) and p.addition(p) == p.scalar_mul(2) for all points p.
     function scalar_mul(G1Point memory p, uint s) internal view returns (G1Point memory r) {
         uint[3] memory input;
@@ -78,7 +78,7 @@ library Pairing {
         bool success;
         // solium-disable-next-line security/no-inline-assembly
         assembly {
-            success := staticcall(sub(gas, 2000), 7, input, 0x80, r, 0x60)
+            success := staticcall(sub(gas(), 2000), 7, input, 0x80, r, 0x60)
             // Use "invalid" to make gas estimation work
             switch success case 0 { invalid() }
         }
@@ -106,7 +106,7 @@ library Pairing {
         bool success;
         // solium-disable-next-line security/no-inline-assembly
         assembly {
-            success := staticcall(sub(gas, 2000), 8, add(input, 0x20), mul(inputSize, 0x20), out, 0x20)
+            success := staticcall(sub(gas(), 2000), 8, add(input, 0x20), mul(inputSize, 0x20), out, 0x20)
             // Use "invalid" to make gas estimation work
             switch success case 0 { invalid() }
         }
@@ -159,6 +159,14 @@ library Pairing {
         return pairing(p1, p2);
     }
 }
+
+
+interface InterfaceVerifier {
+    function verifyProofSender(uint[2] memory a,uint[2][2] memory b,uint[2] memory c,uint[1] memory input) external view returns (bool r);
+    function verifyProofReceiver(uint[2] memory a,uint[2][2] memory b,uint[2] memory c,uint[1] memory input) external view returns (bool r);
+
+}
+
 contract Verifier {
     using Pairing for *;
     struct VerifyingKey {
@@ -183,9 +191,19 @@ contract Verifier {
         vk.IC[1] = Pairing.G1Point(7794051205636092045529470609072859951547433461870270363369500330600295706512,19975551404136004449950966875759663095241423043256189821695219406440003380865);
 
     }
-    function verify(uint[] memory input, Proof memory proof) internal view returns (uint) {
+    function verifyingKeyReceiver() internal pure returns (VerifyingKey memory vk) {
+        vk.alfa1 = Pairing.G1Point(14811349399222038654967707403219934623830058778604097326460601569083410451467,11888959262325394444474583890857068632648829038565396761869597227428988821646);
+        vk.beta2 = Pairing.G2Point([13555356371844350815484211764924768484211669533685500176483553192730624189411,12530047630701454869524008565645840339170275781692944186278211777710302288398], [3520188474172384035710675737199930961814757050865983748010460981865163502768,6691011057598048039298174684931913672234804508862061455666703086472016502341]);
+        vk.gamma2 = Pairing.G2Point([21098632723919929084963287975182345308255893907461951631765085231459365605885,642154386384461916596896121460357144553268794904701754220796376506949330715], [11595898049479758875629277571260380768777742761692764896201793380251594526877,12122276708538582857553586932328898897932334128855061136536225382389056259992]);
+        vk.delta2 = Pairing.G2Point([21326604012661219482995550195827401796673405588407525909330804584207475706478,13381237680005584367691060005175487844793346371492567771268630736017168228077], [5332057464312985521982799419407959174876281949176731286361914755765379205094,13915219617906748426725865221465813298163288930625066368500889626247067445809]);
+        vk.IC = new Pairing.G1Point[](2);
+        vk.IC[0] = Pairing.G1Point(14747726068870132231167237738232759602397734711256551241886301922350563596797,17171152605395321742939573617418623409479201624590221494620285628841985068197);
+        vk.IC[1] = Pairing.G1Point(7762135322944401203483855032847218831791436454510020835432206666191883086198,19593582127578816496279673622024984907369167319952150514495576769383557869862);
+
+    }
+    function verify(uint[] memory input, Proof memory proof, VerifyingKey memory _vk) internal view returns (uint) {
         uint256 snark_scalar_field = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
-        VerifyingKey memory vk = verifyingKey();
+        VerifyingKey memory vk = _vk;
         require(input.length + 1 == vk.IC.length,"verifier-bad-input");
         // Compute the linear combination vk_x
         Pairing.G1Point memory vk_x = Pairing.G1Point(0, 0);
@@ -202,7 +220,8 @@ contract Verifier {
         )) return 1;
         return 0;
     }
-    function verifyProof(
+    
+    function verifyProofSender(
             uint[2] memory a,
             uint[2][2] memory b,
             uint[2] memory c,
@@ -216,10 +235,32 @@ contract Verifier {
         for(uint i = 0; i < input.length; i++){
             inputValues[i] = input[i];
         }
-        if (verify(inputValues, proof) == 0) {
+        if (verify(inputValues, proof, verifyingKey()) == 0) {
             return true;
         } else {
             return false;
         }
+        
+    }
+    function verifyProofReceiver(
+            uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[1] memory input
+        ) public view returns (bool r) {
+        Proof memory proof;
+        proof.A = Pairing.G1Point(a[0], a[1]);
+        proof.B = Pairing.G2Point([b[0][0], b[0][1]], [b[1][0], b[1][1]]);
+        proof.C = Pairing.G1Point(c[0], c[1]);
+        uint[] memory inputValues = new uint[](input.length);
+        for(uint i = 0; i < input.length; i++){
+            inputValues[i] = input[i];
+        }
+        if (verify(inputValues, proof, verifyingKeyReceiver()) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+        
     }
 }
